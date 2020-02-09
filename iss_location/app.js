013078ffee626953d9497a95ed92cbe173a04834
wbrink <will.brink55@gmail.com>
2020-02-08 23:20:57 -0800

    line is made to track location of ISS

--- iss_location/app.js
+++ iss_location/app.js
@@ -6,33 +6,75 @@
 
 var Nasakey = "11yQc9WkVjHeeL8IJidQNKHLs6nlOzDY2UvKSo0W"
 var queryURL = "http://api.open-notify.org/iss-now.json"
+var currentLocation; // array [latitude, longitude]
+var prevLocation; // [latitude, longitude]
+
 
 // setting up the map
-var myMap = L.map("mapid").setView([51.505, -0.09], 3);
+var map = L.map("mapid").setView([38.575764, -121.478851], 9); // parameters: [lat, long], zoom
 
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//   maxZoom: 19,
-//   attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-// }).addTo(myMap);
-
-L.tileLayer('https://www.osmap.us/#2/24.0/-47.1', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-}).addTo(myMap);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(map);
 
 
+// uses api call to make path for ISS
 function callAPI() {
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(res) {
     if (!(res.message === "success")) {
-      console.log("there seems to be an error");
+      //console.log("there seems to be an error");
+      return;
     }
-    console.log(res);
+    //console.log(res);
+    //console.log([parseFloat(res.iss_position.latitude), parseFloat(res.iss_position.longitude)]);
+
+    // if first run then we want two lines to be equal to create starting point
+    if (currentLocation) {
+      prevLocation = currentLocation;
+      currentLocation = [parseFloat(res.iss_position.latitude), parseFloat(res.iss_position.longitude)];
+    } else {
+      prevLocation = currentLocation;
+      currentLocation = [parseFloat(res.iss_position.latitude), parseFloat(res.iss_position.longitude)];
+      prevLocation = [parseFloat(res.iss_position.latitude), parseFloat(res.iss_position.longitude)];
+
+      map.setView(currentLocation, 3) // sets view to where the first 
+    }
+    
+    addLine(prevLocation, currentLocation);
+    // var x = currentLocation;
+    // addCircle(x);
   })
 }
 
+
+// adds between previous location and current location
+function addLine(prevLocation, currentLocation) {
+  var latlngs = [prevLocation, currentLocation];
+  var polyline = L.polyline(latlngs, {
+    color: 'red',
+    weight: 5,
+    opacity: 0.7,
+
+  }).addTo(map);
+
+}
+
+// // make circles
+// function addCircle(x) {
+//   L.circle(x, {
+//     radius: 15000,
+//     opacity: 0.5
+//   }).addTo(map);
+// }
+
+
+callAPI(); // get initial call
 var interval = setInterval(function() {
   callAPI();
 }, 5000);
+
